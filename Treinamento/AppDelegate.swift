786f318a49982b46e9dd3dfa83656f9bef0b33a9
administrador <administrador@Mac-mini-de-administrador-3.local>
2018-07-11 12:08:22 -0300

    Inicial commit

--- Treinamento/AppDelegate.swift
+++ Treinamento/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+// Classe Principal do aplicativo
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- Treinamento/ViewController.swift
+++ Treinamento/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelLogin: UILabel!
+    @IBOutlet weak var matriculaLabel: UIView!
+    @IBOutlet weak var senhaLabel: UIView!
+    @IBOutlet weak var matriculaLegend: UITextField!
+    @IBOutlet weak var senhaLegend: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var matriculaResult: UILabel!
+    @IBOutlet weak var senhaResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.labelLogin.text = "LOGIN"
+        self.matriculaLegend.text = "Entrar com a Matrícula"
+        self.senhaLegend.text = "Senha"
+        self.matriculaLegend.text = "Matrícula Digitada"
+        self.senhaLegend.text = "Senha Digitada"
+        
+        self.enterButton.setTitle("Entrar", for: .normal)
+        self.enterButton.setTitle("Entrando", for: .highlighted)
+        self.enterButton.backgroundColor = .orange
+        self.enterButton.layer.cornerRadius = 5
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 10){
+            
+            self.view.backgroundColor = .red
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
